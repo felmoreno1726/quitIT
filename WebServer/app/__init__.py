894152ba6ded9341d18294d544003144760107bd
felmoreno1726 <felipemoreno1626@gmail.com>
2019-09-14 15:45:18 -0400

    on_join method for logging active users

--- WebServer/app/__init__.py
+++ WebServer/app/__init__.py
@@ -6,10 +6,12 @@ def register_blueprints(app):
     """
     Calls the different modules of the server to initialize them
     """
+    from app.user_endpoit import bp as user_endpoint_bp
+    app.register_blueprint(user_endpoint_bp)
 
 app = Flask(__name__)
 logger = app.logger
 socketio = SocketIO(app, logger=logger)
-#Shows devices currently logged in to the system
+#Session shows user logged into the system using the app
 session = {}
 register_blueprints(app)
--- WebServer/app/user_endpoint/routes.py
+++ WebServer/app/user_endpoint/routes.py
@@ -16,6 +16,7 @@ def handle_alert_message(message):
     and then forwards an SMS alert to the user's phone
     """
     sid = request.sid
+    user_context = session.get(sid)
     
 @socketio.on('doctor_help', namespace=user_namespace)
 def handle_doctor_help(message):
@@ -30,4 +31,14 @@ def handle_update_location(message):
     Checks if the location matches that of a red zone, if yes
     then sends an SMS alert to the user's phone.
     """
-
+socketio.on('join', namespace=user_namespace)
+def on_join(data):
+    """
+    """
+    logger.info("Joining room. Data: " + str(data))
+    username = data["username"]
+    phone_number = data["phone_number"]
+    join_room(username)
+    sid = request.sid
+    session[sid] = {"username": username, "phone_number" : phone_number}
+    logger.info("Session updated\n Current session: " + str(session))
