995aba86d22c26da0323a14609c0799f12d92e38
felmoreno1726 <felipemoreno1626@gmail.com>
2019-09-14 15:28:48 -0400

    Added skeleton for methods to handle the user_endpoint requests

--- WebServer/app/user_endpoint/routes.py
+++ WebServer/app/user_endpoint/routes.py
@@ -6,15 +6,28 @@ from flask_socketio import emit, send, join_room
 from app import socketio, logger, session, context
 from app.devices import bp
 
-user_namespace = "/user_endpoint/channel"
+user_namespace = "/user_endpoint"
 
 @socketio.on('alert', namespace=user_namespace)
 def handle_alert_message(message):
     """
-    Handles alert message.
-    Processes the alert, logs the location to the database
-    and then forwards an alert message to the user's MobileApp
+    Handles alert message comming from RaspberryPi.
+    Processes the alert, logs the location to t database
+    and then forwards an SMS alert to the user's phone
     """
     sid = request.sid
     
-    
+@socketio.on('doctor_help', namespace=user_namespace)
+def handle_doctor_help(message):
+    """
+    Handles a request from the MobileApp to get the user professional help
+    """
+
+@socketio.on('current_location', namespace=user_namespace)
+def handle_update_location(message):
+    """
+    Handles a MobileApp request to update geographical location
+    Checks if the location matches that of a red zone, if yes
+    then sends an SMS alert to the user's phone.
+    """
+
--- WebServer/run.sh
+++ WebServer/run.sh
@@ -1,4 +1,8 @@
-from app import app, socketio
-from flask_socketio import SocketIO
-
-socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+export FLASK_APP=app
+export FLASK_ENV=development
+thisDir=$(cd $(dirname "$0") && pwd)
+#rootDir=$(cd .. && pwd)
+#commonDir=$(cd ../common && pwd)
+#export PYTHONPATH=$PYTHONPATH:$thisDir:$rootDir:$commonDir
+#flask run --host=0.0.0.0
+python3 "${thisDir}/run.py"
