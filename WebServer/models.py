1b496d4a0e1e60da91dc8543959bb7dfae88e3c9
linda413 <caiqinli99@yahoo.com.tw>
2019-09-14 14:27:56 -0700

    create alert

--- WebServer/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-
-class Alert(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.String(100), nullable=False)
-    coordination = db.Column(db.String(100), nullable=False)
