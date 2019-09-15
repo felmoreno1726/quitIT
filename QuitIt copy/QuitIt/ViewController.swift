7f2b0ba7d8713161ede9023d123d20e1f1c1e0b2
bl195 <bl195@duke.edu>
2019-09-14 20:08:52 -0400

    swift

---
+++ QuitIt copy/QuitIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuitIt
+//
+//  Created by Brian Li on 9/14/19.
+//  Copyright © 2019 Brian Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
