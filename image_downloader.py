a98c99feb29eb22820a14c28c12b275ee65015db
Abhi Jadhav <aj246@duke.edu>
2019-09-14 20:11:44 -0400

    Added training data to gitignore

--- image_downloader.py
+++ image_downloader.py
@@ -1,3 +1,7 @@
 from google_images_download import google_images_download  
 
 response = google_images_download.googleimagesdownload() 
+
+arguments = {"keywords":"Polar bears,baloons,Beaches","limit":20,"print_urls":True}   #creating list of arguments
+paths = response.download(arguments)   #passing the arguments to the function
+print(paths)   #printing absolute paths of the downloaded images
